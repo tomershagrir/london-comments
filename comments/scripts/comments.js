9d342a62a06f56ebfb7a1384f312b41432c0d4f8
bisibuka <bisibuka@gmail.com>
2012-06-06 15:29:41 +0300

    comments render bug fix

--- comments/scripts/comments.js
+++ comments/scripts/comments.js
@@ -3,16 +3,18 @@ $(document).ready(function(){
 	var ownerIDInput = $('#owner_id');
 	var ownerModelInput = $('#owner_model');
 	var comments = $('#comments');
+	var link = null;
 
 	var newCommentHTML = '<div class="comment" style="margin-left:${margin}px;"><b>${author}</b> <i>said</i>: "${comment}" at <i>${date}</i><br/><a class="reply-link" id="${id}">reply</a></div>';
 
 	comments.delegate('.reply-link, .add-comment', 'click', function(){
-		var link = $(this);
+		link = $(this);
 		
 		commentForm.appendTo(link.parent()).show();
 		ownerIDInput.val(OWNER_ID);
 		ownerModelInput.val(OWNER_MODEL);
 		addToRoot = true;
+		insertAfter = null;
 		if(link.hasClass('reply-link')) {
 			addToRoot = false;
 			link.hide();
@@ -20,7 +22,10 @@ $(document).ready(function(){
 			ownerIDInput.val(link.attr('id'));
 			ownerModelInput.val('Comment');
 		}
-		else insertAfter = comments.children('.comment').last();
+		else {
+			var childrenComments = comments.children('.comment');
+			if(childrenComments.length) insertAfter = childrenComments.last();
+		}
 	});
 
 	$('#send_comment_btn').click(function(){
@@ -31,14 +36,18 @@ $(document).ready(function(){
 		$.post('/comments/post/', {'owner_id': ownerIDInput.val(), 'owner_model': ownerModelInput.val(), 'body': textArea.val()}).success(function(response){
 			if(response.status != "ok") return;
 			var newCommentMargin = addToRoot ? 0 : parseInt(insertAfter.css('margin-left'))+25;
-			$.tmpl(newCommentHTML, {'author': response.author, 
+			var newComment = $.tmpl(newCommentHTML, {'author': response.author, 
 									'comment': response.body, 
 									'date': response.created,
 									'id': response.pk,
-									'margin': newCommentMargin}).insertAfter(insertAfter); // shift subcomment
+									'margin': newCommentMargin}); // shift subcomment
+			
+			if(insertAfter) newComment.insertAfter(insertAfter)
+			else newComment.prependTo(comments);
 			
 			textArea.val('');
 			commentForm.hide();
+			link.show();
 		}).error(function(response){
 			response = $.parseJSON(response.responseText);
 			alert(response.error);
