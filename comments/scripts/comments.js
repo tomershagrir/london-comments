4b87358112455b7a93e9d05e2e118baacaa070d5
Marinho Brandao <marinho@gmail.com>
2012-09-28 11:38:59 +0200

    Changes for getting more flexible and customisable code

--- comments/scripts/comments.js
+++ comments/scripts/comments.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 	var newCommentHTML = '<div class="comment" style="margin-left:${margin}px;"><b>${author}</b> <i>said</i>: "${comment}" at <i>${date}</i><br/><a class="reply-link" id="${id}">reply</a></div>';
 
 	comments.delegate('.reply-link, .add-comment', 'click', function(){
-        console.log('delegate'); // XXX
 		link = $(this);
 		
 		commentForm.appendTo(link.parent()).show();
@@ -29,22 +28,7 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$('#send_comment_btn').click(function(){
-        comments_send_function(commentForm, '/comments/post/', true, addToRoot, newCommentHTML, link);
-	});
-});
-
-function comments_send_function(form, url, hideForm, addToRoot, newCommentHTML, link) {
-    var textArea = form.find('#comment_text');
-    if (textArea.val().trim() == "") return;
-    var ownerId = form.find('#owner_id').val();
-    var ownerModel = form.find('#owner_model').val();
-    var insertAfter = null; // XXX
-    
-    console.log([form, url, ownerId, ownerModel, hideForm]); // XXX
-
-    //TODO: replace with serialize()
-    $.post(url, {'owner_id': ownerId, 'owner_model': ownerModel, 'body': textArea.val()}).success(function(response){
+    function commentCallback(response) {
         if(response.status != "ok") return;
         var newCommentMargin = addToRoot ? 0 : parseInt(insertAfter.css('margin-left'))+25;
         var newComment = $.tmpl(newCommentHTML, {'author': response.author, 
@@ -57,11 +41,32 @@ function comments_send_function(form, url, hideForm, addToRoot, newCommentHTML, 
         else newComment.prependTo(comments);
         
         textArea.val('');
-        if (hideForm) form.hide();
-        if (link) link.show();
-    }).error(function(response){
+        form.hide();
+        link.show();
+    }
+
+    function errorCallback(response){
         response = $.parseJSON(response.responseText);
         alert(response.error);
-    });
+    }
+
+	$('#send_comment_btn').click(function(){
+        comments_send_function(commentForm, '/comments/post/', commentCallback, errorCallback);
+	});
+});
+
+function comments_send_function(form, url, successCallback, errorCallback, extra_params) {
+    var textArea = form.find('#comment_text');
+    if (textArea.val().trim() == "") return;
+    var ownerId = form.find('#owner_id').val();
+    var ownerModel = form.find('#owner_model').val();
+    var params = {'owner_id': ownerId, 'owner_model': ownerModel, 'body': textArea.val()};
+    if (extra_params) {
+        for (var k in extra_params)
+            params[k] = extra_params[k];
+    }
+
+    //TODO: replace with serialize()
+    $.post(url, params).success(successCallback).error(errorCallback);
 }
 
--- comments/views.py
+++ comments/views.py
@@ -14,7 +14,6 @@ SUBCOMMENT_LEFT_MARGIN = 25
 
 #function to render comments template
 def render_comments(owner, list_template='comments', item_template='comment', margin=SUBCOMMENT_LEFT_MARGIN):
-    print('1', list_template, item_template) # XXX
     model_name = owner._meta.verbose_name
     if model_name in MODELS_WITH_COMMENTS:
         return render_to_string(list_template, {
@@ -25,7 +24,6 @@ def render_comments(owner, list_template='comments', item_template='comment', ma
 
 #function to render comments with sub-comments
 def get_comments_html(owner, result="", level=0, item_template='comment', margin=SUBCOMMENT_LEFT_MARGIN):
-    print('2', item_template) # XXX
     comments = Comment.query().filter(owner=owner)
     for comment in comments:
         result += get_comments_html(comment, get_single_comment_html(comment, level, item_template, margin), level+1, item_template, margin) #recursively render subcomments
@@ -33,20 +31,27 @@ def get_comments_html(owner, result="", level=0, item_template='comment', margin
     
 #function to render single comment
 def get_single_comment_html(comment, level, template='comment', margin=SUBCOMMENT_LEFT_MARGIN):
-    print('3', template) # XXX
     return render_to_string(template, {'comment': comment, 'margin': level * margin})
 
 #function to receive comment posting AJAX request 
 def post_comment(request):
     try:
-        body, model_name, owner_pk = request.POST.values()
+        body = request.POST['body']
+        model_name = request.POST['owner_model']
+        owner_pk = request.POST['owner_id']
+
         owner_model = get_model("%s.%s" % (MODELS_WITH_COMMENTS[model_name], model_name))
         owner = owner_model.query().get(pk = owner_pk)
         author = request.user if request.user.is_authenticated() else None
         comment = Comment.query().create(owner = owner, body=body, author = author)
-        return JsonResponse('{"status":"ok", "pk":"%s", "author":"%s", "body":"%s", "created":"%s"}' % 
-                            (comment['pk'], comment['author'] or "Anonymous", comment['body'], comment['created'].strftime('%Y-%m-%d, %H:%M')))
+        return JsonResponse({
+            "status":"ok",
+            "pk": comment['pk'],
+            "author": unicode(comment['author']) if comment['author'] else "Anonymous",
+            "body": comment['body'],
+            "created": comment['created'].strftime('%Y-%m-%d, %H:%M'),
+            })
     except Exception as e:
-        response = JsonResponse('{"status":"error", "error":"%s"}' % str(e).replace('"', '\\"'))
+        response = JsonResponse({"status":"error", "error":str(e).replace('"', '\\"')})
         response.status_code = 500
         return response
