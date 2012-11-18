ef318c4a9ee7cf865d55bcb1233e68f61cb566b9
Danil Galeev <digaleev@gmail.com>
2012-11-18 12:54:44 +0400

    Comments app should not know that there are office operator in request

--- comments/views.py
+++ comments/views.py
@@ -31,32 +31,15 @@ def get_single_comment_html(comment, level, theme):
     return render_to_string('comment', {'comment': comment, 'margin': level*SUBCOMMENT_LEFT_MARGIN}, theme=theme) #FIXME: 
 
 #function to receive comment posting AJAX request 
-def post_comment(request):
+def post_comment(request, operator=None):
     try:
         body, model_name, owner_pk = request.POST.values()
         owner_model = get_model("%s.%s" % (MODELS_WITH_COMMENTS[model_name], model_name))
         owner = owner_model.query().get(pk = owner_pk)
         author = request.user if request.user.is_authenticated() else None
-        comment = Comment.query().create(owner = owner, body=body, author = author)
-        return JsonResponse('{"status":"ok", "pk":"%s", "author":"%s", "body":"%s", "created":"%s"}' % 
-                            (comment['pk'], comment['author'] or "Anonymous", comment['body'], comment['created'].strftime('%Y-%m-%d, %H:%M')))
-    except Exception as e:
-        response = JsonResponse('{"status":"error", "error":"%s"}' % str(e).replace('"', '\\"'))
-        response.status_code = 500
-        return response
-
-#function to receive comment from operators only posting AJAX request
-def post_operator_comment(request):
-    try:
-        MODELS_WITH_COMMENTS = getattr(settings, 'MODELS_WITH_COMMENTS', {})
-        MODELS_WITH_COMMENTS.update({'Comment': 'comments'})
-        body, model_name, owner_pk = request.POST.values()
-        owner_model = get_model("%s.%s" % (MODELS_WITH_COMMENTS[model_name], model_name))
-        owner = owner_model.query().get(pk = owner_pk)
-        operator = request.office_operator
-        comment = Comment.query().create(owner = owner, body=body, operator=operator)
-        return JsonResponse('{"status":"ok", "pk":"%s", "operator":"%s", "body":"%s", "created":"%s"}' %
-                            (comment['pk'], operator['name'] or operator['username'], comment['body'], comment['created'].strftime('%Y-%m-%d, %H:%M')))
+        comment = Comment.query().create(owner = owner, body=body, author = author, operator = operator)
+        return JsonResponse('{"status":"ok", "pk":"%s", "author":"%s", "operator":"%s", "body":"%s", "created":"%s"}' %
+                            (comment['pk'], comment['author'] or "Anonymous", comment['operator'], comment['body'], comment['created'].strftime('%Y-%m-%d, %H:%M')))
     except Exception as e:
         response = JsonResponse('{"status":"error", "error":"%s"}' % str(e).replace('"', '\\"'))
         response.status_code = 500
