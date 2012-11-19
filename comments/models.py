a766e1c1e9bea3713ef142e9e2f27f78c48cc567
Danil Galeev <digaleev@gmail.com>
2012-11-19 18:01:40 +0400

    operator deleted from model and view

--- comments/models.py
+++ comments/models.py
@@ -8,8 +8,8 @@ class Comment(models.Model):
     Stores comments for different content types
     """
     owner = models.ForeignKey(related_name="comments", delete_cascade=True)
-    operator = models.Field(null=True, blank=True, default=None)
-    author = models.ForeignKey('auth.User', related_name="comments", default=None, blank=True, null=True, delete_cascade=True)
+    author = models.Field(null=True, blank=True, default=None)
+#    author = models.ForeignKey('auth.User', related_name="comments", default=None, blank=True, null=True, delete_cascade=True)
     created = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
     
