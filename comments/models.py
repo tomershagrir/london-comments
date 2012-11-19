97304e5f72bad669fbc36187982c8ed5cd87acc1
Marinho Brandao <marinho@gmail.com>
2012-11-19 18:31:37 +0100

    Method to filter comments by author

--- comments/models.py
+++ comments/models.py
@@ -3,12 +3,21 @@ from datetime import datetime
 from london.utils.safestring import mark_safe
 from london.db import models
 
+
+class CommentQuerySet(models.QuerySet):
+    def by_author(self, author):
+        return self.filter(author=author)
+
+
 class Comment(models.Model):
     """
     Stores comments for different content types
     """
+    class Meta:
+        query = 'comments.models.CommentQuerySet'
+
     owner = models.ForeignKey(related_name="comments", delete_cascade=True)
-    author = models.Field(null=True, blank=True, default=None)
+    author = models.AnyField(null=True, blank=True, default=None)
 #    author = models.ForeignKey('auth.User', related_name="comments", default=None, blank=True, null=True, delete_cascade=True)
     created = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
